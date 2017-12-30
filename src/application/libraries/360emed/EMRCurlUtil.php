4142aba734e80f5bbc1b223c6865d1a42cbe70c1
Huming Tang <tangh02@med.nyu.edu>
2017-12-29 20:04:59 -0500

    debug

--- src/application/libraries/360emed/EMRCurlUtil.php
+++ src/application/libraries/360emed/EMRCurlUtil.php
@@ -29,7 +29,8 @@ class EMRCurlUtil
 
         // set URL and other appropriate options
         curl_setopt($ch, CURLOPT_URL, $this->authRefreshURL);
-        curl_setopt($ch, CURLOPT_HEADER, array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_HEADER, array('Content-Type: application/json',
+                                               'Content-Length: ' . strlen($authBody)));
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_POSTFIELDS, $authBody);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
